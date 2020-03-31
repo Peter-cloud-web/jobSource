e280637659d4021824a524a0fd0ea927218fea8b
Peter-cloud-web <petergichia165@gmail.com>
2020-03-31 03:55:45 +0300

    provide a name for the content provide which is the package name for the app

--- app/src/main/java/com/moringa/jobsource/Data/jobSourceContract.java
+++ app/src/main/java/com/moringa/jobsource/Data/jobSourceContract.java
@@ -1,4 +1,24 @@
 package com.moringa.jobsource.Data;
 
+import android.provider.BaseColumns;
+
 public class jobSourceContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.moringa.jobsource";
+
+    private jobSourceContract(){}
+
+    public static final class RegisterEntry implements BaseColumns{
+        public final static String TABLE_NAME = "register";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_FIRST_NAME = "firstname";
+        public final static String COLUMN_LAST_NAME = "lastname";
+        public final static String COLUMN_EMAIL_NAME = "email";
+        public final static String COLUMN_PASSWORD_NAME = "password";
+
+
+    }
+
+
 }
